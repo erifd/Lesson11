862a74c6d9c3fb2fabd8fcd25195fa3b09cb13b9
erifd <114823398+erifd@users.noreply.github.com>
2023-12-11 18:00:58 -0800

    Add files via upload

---
+++ Lesson11_Problem1.py
@@ -0,0 +1,9 @@
+
+print("Problem 1")
+print("----------")
+
+word = input("Give me a Word: ")
+print("The length of", word, "is", len(word), "letters.")
+print("The first letter of this word is", word[0])
+print("The last letter of this word is", len(word) - 1)
+print("The next to last letter of this word is", len(word) - 2)
